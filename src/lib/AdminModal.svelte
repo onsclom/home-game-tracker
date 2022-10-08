7cf6cfdda6f0245deea9d772890aeb9ac74a4720
Austin Merrick <austinmerrick98@gmail.com>
2022-10-08 13:01:24 -0700

    Add admin modal

---
+++ src/lib/AdminModal.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { ledger } from '$lib/stores';
+	import Modal from '$lib/Modal.svelte';
+
+	export let visible = false;
+	let adminJSON = JSON.stringify($ledger, null, 2);
+
+	function updateFromAdmin() {
+		navigator.clipboard.writeText(adminJSON);
+		$ledger = JSON.parse(adminJSON);
+		visible = false;
+	}
+</script>
+
+<Modal bind:visible>
+	<p>if you are not austin then pppoof please</p>
+	<form on:submit|preventDefault={updateFromAdmin}>
+		<textarea bind:value={adminJSON} />
+		<input type="submit" value="update" />
+		<form />
+	</form>
+</Modal>
+
+<style>
+</style>
