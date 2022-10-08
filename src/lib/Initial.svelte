8da635ef4379075b227ef2445a79e8e48fa3d180
Austin Merrick <austinmerrick98@gmail.com>
2022-10-08 13:01:54 -0700

    Fix input mode

--- src/lib/Initial.svelte
+++ src/lib/Initial.svelte
@@ -15,7 +15,7 @@
 	<label for="player-names-textarea">player names (seperated by new lines):</label>
 	<textarea bind:value={nameText} id="player-names-textarea" />
 	<label for="starting-buyin-input">starting buy in:</label>
-	<input bind:value={startBuyin} id="starting-buyin-input" type="decimal" />
+	<input bind:value={startBuyin} id="starting-buyin-input" inputmode="decimal" />
 	<input type="submit" value="start session" />
 </form>
 
