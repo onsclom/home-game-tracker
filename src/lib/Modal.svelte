f4b284c799dbf6ed8689356533ee941bce0cec5c
Austin Merrick <austinmerrick98@gmail.com>
2022-10-07 23:46:33 -0700

    Add modal component

---
+++ src/lib/Modal.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	export let visible = false;
+
+	function hide() {
+		visible = false;
+	}
+
+	function handleKeydown(event: KeyboardEvent) {
+		if (event.key === 'Escape') {
+			hide();
+		}
+	}
+</script>
+
+<svelte:window on:keydown={handleKeydown} />
+
+{#if visible}
+	<div class="background" on:click={hide}>
+		<div class="inner" on:click|stopPropagation>
+			<button on:click={hide}>exit</button>
+			<div class="content">
+				<slot />
+			</div>
+		</div>
+	</div>
+{/if}
+
+<style>
+	.background {
+		position: absolute;
+		display: flex;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-color: rgba(0, 0, 0, 0.5);
+	}
+
+	.inner {
+		position: relative;
+		margin: auto;
+		padding: 1rem;
+		background-color: white;
+	}
+
+	button {
+		position: absolute;
+		margin: 0;
+		top: 1rem;
+		right: 1rem;
+	}
+
+	.content {
+		padding: 2rem;
+	}
+</style>
