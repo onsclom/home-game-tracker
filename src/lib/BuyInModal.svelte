8b64d9dde25ca7a2cbecd302a51100af07712589
Austin Merrick <austinmerrick98@gmail.com>
2022-10-08 13:19:01 -0700

    Add buy in modal

---
+++ src/lib/BuyInModal.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { ledger } from '$lib/stores';
+	import Modal from '$lib/Modal.svelte';
+
+	export let visible = false;
+
+	function handleSumbit() {}
+</script>
+
+<Modal bind:visible>
+	<h2>buy in</h2>
+	<form on:submit|preventDefault={handleSumbit}>
+		<button>existing player</button>
+		<span>OR</span>
+		<button>new player</button>
+	</form>
+</Modal>
+
+<style>
+</style>
