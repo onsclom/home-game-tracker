c3c822240612353ad7c0ec05260acddf83cf0e2a
Austin Merrick <austinmerrick98@gmail.com>
2022-10-09 18:17:46 -0700

    Remove players store and refactor stores

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,10 +1,14 @@
 import { browser } from '$app/environment';
 import { writable, type Writable } from 'svelte/store';
 
-export const players: Writable<string[]> = writable([]);
-const ledgerVal = browser && localStorage.ledger ? JSON.parse(localStorage.ledger) : [];
-export const ledger: Writable<Entry[]> = writable(ledgerVal);
-if (browser) ledger.subscribe((ledger) => (localStorage.ledger = JSON.stringify(ledger)));
-const stateVal = browser && !isNaN(+localStorage.state) ? +localStorage.state : 0;
-export const state: Writable<number> = writable(stateVal);
-if (browser) state.subscribe((state) => (localStorage.state = +state));
+export const ledger: Writable<Entry[]> = writable(
+	browser && localStorage.ledger ? JSON.parse(localStorage.ledger) : []
+);
+export const state: Writable<number> = writable(
+	browser && !isNaN(+localStorage.state) ? +localStorage.state : 0
+);
+
+if (browser) {
+	ledger.subscribe((ledger) => (localStorage.ledger = JSON.stringify(ledger)));
+	state.subscribe((state) => (localStorage.state = +state));
+}
