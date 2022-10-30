f972119c8fd841fa9fabc191b709a3697e87a06a
chintanbhatt2 <59454532+chintanbhatt2@users.noreply.github.com>
2022-10-30 15:26:00 -0700

    fixes text color

--- src/routes/(session)/session/[session_name]/+page.svelte
+++ src/routes/(session)/session/[session_name]/+page.svelte
@@ -48,41 +48,43 @@
 	});
 </script>
 
-<div
-	class="flex shrink h-screen max-h-screen grow-0 flex-col md:flex-row bg-slate-900 text-slate-300"
->
+<div class="text-slate-300">
 	<div
-		class="h-56 md:h-full flex shrink flex-row md:flex-col md:overflow-hidden pl-4 md:pr-10 md:pb-10 pb-5"
+		class="flex shrink h-screen max-h-screen grow-0 flex-col md:flex-row bg-slate-900 text-slate-300"
 	>
-		<div class="w-48">
-			<a href="/" class="text-2xl underline"> Home </a>
-			<div class="font-thin italic tracking-widest text-3xl ">
-				{time.toLocaleTimeString()}
+		<div
+			class="h-56 md:h-full flex shrink flex-row md:flex-col md:overflow-hidden pl-4 md:pr-10 md:pb-10 pb-5"
+		>
+			<div class="w-48">
+				<a href="/" class="text-2xl underline"> Home </a>
+				<div class="font-thin italic tracking-widest text-3xl ">
+					{time.toLocaleTimeString()}
+				</div>
+			</div>
+			<div class="h-full pb-10">
+				<History />
 			</div>
 		</div>
-		<div class="h-full pb-10">
-			<History />
-		</div>
-	</div>
-	<div class="basis-auto flex flex-col shrink w-full overflow-hidden">
-		<div class="flex flex-col">
-			<div>
-				<button
-					on:click={() => {
-						showAdminModal = true;
-					}}>admin panel</button
-				>
+		<div class="basis-auto flex flex-col shrink w-full overflow-hidden">
+			<div class="flex flex-col">
+				<div>
+					<button
+						on:click={() => {
+							showAdminModal = true;
+						}}>admin panel</button
+					>
+				</div>
+				<h2><b>${$tableSum.toFixed(2)}</b> <span>on table</span></h2>
 			</div>
-			<h2><b>${$tableSum.toFixed(2)}</b> <span>on table</span></h2>
+			<PlayersList {newPlayer} {buyIn} {cashOut} />
 		</div>
-		<PlayersList {newPlayer} {buyIn} {cashOut} />
 	</div>
-</div>
 
-<BuyInModal {selectedPlayer} bind:visible={showBuyInModal} />
-<CashOutModal {selectedPlayer} bind:visible={showCashOutModal} />
-<NewPlayerModal bind:visible={showNewPlayerModal} />
-<AdminModal bind:visible={showAdminModal} />
+	<BuyInModal {selectedPlayer} bind:visible={showBuyInModal} />
+	<CashOutModal {selectedPlayer} bind:visible={showCashOutModal} />
+	<NewPlayerModal bind:visible={showNewPlayerModal} />
+	<AdminModal bind:visible={showAdminModal} />
+</div>
 
 <style>
 </style>
